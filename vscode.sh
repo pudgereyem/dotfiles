b8779e1bf07186dcc2e097473d01c65316d9ad61
Victor Meyer <victor.reyem@gmail.com>
2021-04-19 23:43:28 +0200

    fix: changes

--- vscode.sh
+++ vscode.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+LINK_TARGET_EXISTS_HANDLING=""
+while true; do
+    read -p "$(echo -e 'If files exist or are already symlinked, do you want to replace?\nAnswer [y]es, [n]o, or [p]rompt: ')" yn
+    case $yn in
+        [Yy]* ) LINK_TARGET_EXISTS_HANDLING="f"; break;;
+        [Nn]* ) LINK_TARGET_EXISTS_HANDLING=""; break;;
+        [Pp]* ) LINK_TARGET_EXISTS_HANDLING="i"; break;;
+        * ) echo "Please answer: ";;
+    esac
+done
+
 # Plugins
 code --install-extension patbenatar.advanced-new-file
 code --install--extension bungcip.better-toml
@@ -15,3 +26,7 @@ code --install-extension alefragnani.project-manager
 code --install--extension bungcip.matklad.rust-analyzer
 code --install--extension whtouche.vscode-js-console-utils
 
+
+# Settings
+ln -sv${LINK_TARGET_EXISTS_HANDLING} "${HOME}/code/dotfiles/.vscode.settings.json" "${HOME}/Library/Application Support/Code/User/settings.json"
+ln -sv${LINK_TARGET_EXISTS_HANDLING} "${HOME}/code/dotfiles/.vscode.keybindings.json" "${HOME}/Library/Application Support/Code/User/keybindings.json"
