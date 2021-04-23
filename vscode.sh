f0d393ed8469a8b0c5d2ce56a96e7b75fe6b63cb
Victor Meyer <victor.reyem@gmail.com>
2021-04-23 10:31:25 +0200

    chore: add more vs code extensions

--- vscode.sh
+++ vscode.sh
@@ -13,18 +13,17 @@ done
 
 # Plugins
 code --install-extension patbenatar.advanced-new-file
-code --install--extension bungcip.better-toml
 code --install-extension wmaurer.change-case
 code --install-extension mikestead.dotenv
 code --install-extension dracula-theme.theme-dracula
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension eamodio.gitlens
-code --install--extension graphql.vscode-graphql
+code --install-extension graphql.vscode-graphql
 code --install-extension esbenp.prettier-vscode
-code --install--extension bungcip.prisma.prisma
 code --install-extension alefragnani.project-manager
-code --install--extension bungcip.matklad.rust-analyzer
-code --install--extension whtouche.vscode-js-console-utils
+code --install-extension whtouche.vscode-js-console-utils
+code --install-extension bradlc.vscode-tailwindcss
+code --install-extension csstools.postcss
 
 
 # Settings
